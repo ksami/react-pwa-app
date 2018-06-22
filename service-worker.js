eda4f65d4fc239d33d0c541becd33552ff5b2b29
ksami <ksami.ihide@gmail.com>
2018-06-22 22:07:30 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 
 const cacheName = "mockPWA-v0.1.0";
-const filesToCache = ["./","./index.html","https://jsonbin.io/b/59f721644ef213575c9f6531","./manifest.json","./icons/android-icon-36x36.png","./icons/android-icon-48x48.png","./icons/android-icon-72x72.png","./icons/android-icon-96x96.png","./icons/android-icon-144x144.png","./icons/android-icon-192x192.png","./icons/android-icon-512x512.png","./icons/favicon.ico","./static/css/main.871c1fe2.css","./static/js/main.d52bd0c7.js"];
+const filesToCache = ["./","./index.html","https://jsonbin.io/b/59f721644ef213575c9f6531","./manifest.json","./icons/android-icon-36x36.png","./icons/android-icon-48x48.png","./icons/android-icon-72x72.png","./icons/android-icon-96x96.png","./icons/android-icon-144x144.png","./icons/android-icon-192x192.png","./icons/android-icon-512x512.png","./icons/favicon.ico","./static/css/main.871c1fe2.css","./static/js/main.6cedc180.js"];
 self.addEventListener('install', function(e) {
   e.waitUntil(caches.open(cacheName)
     .then(function(cache) {
